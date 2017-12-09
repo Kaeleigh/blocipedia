7770d998af907f9c1581c9916dd3712828cfbd97
[] <awh938@gmail.com>
2017-12-09 15:10:08 -0500

    CRUD done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :wikis
+
+  validates :email, presence: true
+  validates :password, presence: true
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email "test@bloc.io"
+    password 'password'
+  end
+end
