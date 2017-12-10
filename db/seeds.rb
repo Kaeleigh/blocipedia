eac6b7e05daa94df6aeb397504a194ada1423d8b
[] <awh938@gmail.com>
2017-12-10 08:01:59 -0500

    add fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,61 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+9.times do
+  User.create!(
+    email:  Faker::Internet.email,
+    password: Faker::Internet.password,
+    role: 'standard'
+  )
+end
+standard_users = User.where(role: 'standard')
+
+4.times do
+  User.create!(
+    email:  Faker::Internet.email,
+    password: Faker::Internet.password,
+    role: 'premium'
+  )
+end
+premium_users = User.where(role: 'premium')
+
+10.times do
+  Wiki.create!(
+    user: standard_users.sample,
+    title:  Faker::HarryPotter.book,
+    body: Faker::HarryPotter.quote,
+    private: false
+  )
+end
+
+10.times do
+  Wiki.create!(
+    user: premium_users.sample,
+    title: Faker::RickAndMorty.character,
+    body: Faker::RickAndMorty.quote,
+    private: Faker::Boolean.boolean
+  )
+end
+
+standard_member = User.create!(
+  email:  'standard@test.com',
+  password: 'password1',
+  role: 'standard'
+)
+
+premium_member = User.create!(
+  email: 'premium@test.com',
+  password: 'password2',
+  role: 'premium'
+)
+
+admin = User.create!(
+  email: 'admin@test.com',
+  password: 'password3',
+  role: 'admin'
+)
+
+puts "Seeds finished"
+puts "#{standard_users.count} standard users created"
+puts "#{premium_users.count} premium users created"
+puts "#{User.where(role: 'admin').count} admins created"
+puts "#{Wiki.count} topics created"
